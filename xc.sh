56f83eb721a5967a6dd4eddfe7e5080d02124af6
git <greywolf@starwolf.com>
2021-05-13 02:20:08 -0700

    Picked files from release/4.0 to dev/5.0:     .

--- xc.sh
+++ xc.sh
@@ -43,7 +43,7 @@ OPTS="lrvR:f:";
 # change these if we need better resolution than this.
 #lMIN=0;
 #lMAX=5;
-VERSION="xc version 3.2 2020-06-12";
+VERSION="xc 4.0 2021-05-13 greywolf@starwolf.com";
 
 # lower and upper midrange
 #(( lMIDL = (lMAX + lMIN) / 2 ))
